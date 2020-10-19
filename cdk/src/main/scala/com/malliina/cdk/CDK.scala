946d3f6add42a922a994fca6e245ec1ce3c7dfbf
Michael Skogberg <malliina123@gmail.com>
2020-10-19 20:08:11 +0300

    Cleanup

--- cdk/src/main/scala/com/malliina/cdk/CDK.scala
+++ cdk/src/main/scala/com/malliina/cdk/CDK.scala
@@ -1,8 +1,6 @@
 package com.malliina.cdk
 
 import com.malliina.cdk.LambdaPipeline.LambdaConf
-import com.malliina.cdk.S3Redirect.RedirectConf
-import com.malliina.cdk.S3WebsiteStack.WebsiteConf
 import software.amazon.awscdk.core.{Environment, StackProps, App => AWSApp}
 
 object CDK {
@@ -15,19 +13,6 @@ object CDK {
   def main(args: Array[String]): Unit = {
     val app = new AWSApp()
 
-    val websiteConf =
-      WebsiteConf("cdk.malliina.site", "/global/route53/zone", "/global/certificates/arn")
-    val website = S3WebsiteStack(websiteConf, app, "cdk-website")
-    val redirect = S3Redirect(
-      RedirectConf(
-        "old.malliina.site",
-        websiteConf.domain,
-        websiteConf.hostedZoneParamName,
-        websiteConf.certificateParamName
-      ),
-      app,
-      "cdk-redirect"
-    )
     val lambda = LambdaStack(app, "LambdaStack")
     val lambdaPipeline = LambdaPipeline(LambdaConf(lambda.getStackId, lambda.code), app, "mylambda")
 
---
+++ lambda/src/test/scala/com/malliina/lambda/TemplateTests.scala
@@ -0,0 +1,9 @@
+package com.malliina.lambda
+
+import munit.FunSuite
+
+class TemplateTests extends FunSuite {
+  test("can run test") {
+    assert(1 == 1)
+  }
+}
