80adb1d219c56481004b13c9af2b8ad811aae239
Michael Skogberg <michael.skogberg@futurice.com>
2023-02-20 21:16:57 +0200

    Add filter

--- cdk/src/main/scala/com/malliina/cdk/LambdaStack.scala
+++ cdk/src/main/scala/com/malliina/cdk/LambdaStack.scala
@@ -1,7 +1,9 @@
 package com.malliina.cdk
 
 import software.amazon.awscdk.{Duration, Stack}
-import software.amazon.awscdk.services.lambda.{CfnParametersCode, Function as LambdaFunction, Runtime as LambdaRuntime}
+import software.amazon.awscdk.services.lambda.{CfnParametersCode, IFunction, Function as LambdaFunction, Runtime as LambdaRuntime}
+import software.amazon.awscdk.services.logs.{FilterPattern, ILogGroup, SubscriptionFilter}
+import software.amazon.awscdk.services.logs.destinations.LambdaDestination
 import software.constructs.Construct
 
 class LambdaStack(scope: Construct, val constructId: String)
@@ -15,4 +17,12 @@ class LambdaStack(scope: Construct, val constructId: String)
     .code(code)
     .memorySize(256)
     .timeout(Duration.seconds(60))
+    .build()
+  val streamLambda: IFunction =
+    LambdaFunction.fromFunctionName(stack, "StreamFunction", "LogsToElasticsearch_search")
+  val filter = SubscriptionFilter.Builder
+    .create(stack, "Filter")
+    .logGroup(function.getLogGroup)
+    .destination(LambdaDestination.Builder.create(streamLambda).build())
+    .filterPattern(FilterPattern.spaceDelimited("timestamp", "level", "logger", "message"))
     .build()
