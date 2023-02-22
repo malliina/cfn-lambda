e81d52a6f29a6ffe89398b8a169d3bcaef199b71
Michael Skogberg <michael.skogberg@futurice.com>
2023-02-22 15:07:07 +0200

    Up pipe

--- cdk/src/main/scala/com/malliina/cdk/PipelinesStack.scala
+++ cdk/src/main/scala/com/malliina/cdk/PipelinesStack.scala
@@ -16,34 +16,26 @@ class PipelinesStack(scope: Construct, stackName: String)
     .repositoryName(getStackName)
     .description(s"Code for $getStackName.")
     .build()
-  val sourceInput = CodePipelineSource.codeCommit(source, "master")
   val pipeline = CodePipeline.Builder
     .create(stack, "Pipeline")
     .pipelineName(getStackName)
     .synth(
       ShellStep.Builder
         .create("Synth")
-        .input(sourceInput)
-        .commands(list(s"npm install -g aws-cdk@$cdkVersion", "cdk synth"))
+        .input(CodePipelineSource.codeCommit(source, "master"))
+        .commands(
+          list(
+            """sbt "project lambda" assembly""",
+            "unzip lambda/function.jar -d jartarget",
+            s"npm install -g aws-cdk@$cdkVersion",
+            "cdk synth"
+          )
+        )
         .build()
     )
     .build()
 
   val lambdaApp = pipeline.addStage(LambdaStage(stack, "qa"))
-  lambdaApp.addPre(
-    ShellStep.Builder
-      .create("Build")
-      .input(sourceInput)
-      .primaryOutputDirectory("jartarget")
-      .commands(
-        list(
-          "sbt sbtVersion",
-          """sbt "project lambda" assembly""",
-          "unzip lambda/function.jar -d jartarget"
-        )
-      )
-      .build()
-  )
 
 class LambdaStage(scope: Construct, id: String) extends Stage(scope, id):
   val stack = LambdaStack(this, "LambdaStack")
