95f59a76fa4aa76053215cbb82c561f1b5af0978
Michael Skogberg <michael.skogberg@futurice.com>
2023-02-23 12:21:42 +0200

    Trickery with envs

--- cdk/build.sh
+++ cdk/build.sh
@@ -1,7 +1,7 @@
 # https://docs.aws.amazon.com/cdk/api/v1/docs/pipelines-readme.html#migrating-from-buildspecyml-files
 echo "Building function to $OUTPUT_DIR"
 sbt "project lambda" assembly
-unzip lambda/function.jar -d "$OUTPUT_DIR"
+unzip lambda/function.jar -d jartarget
 echo "Running synth with CDK $CDK_VERSION"
-npm install -g aws-cdk@"$CDK_VERSION"
+npm install -g aws-cdk
 cdk synth
