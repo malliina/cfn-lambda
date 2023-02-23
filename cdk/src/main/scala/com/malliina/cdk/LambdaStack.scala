4a791a4186943136bdb70da4203055869fb24614
Michael Skogberg <michael.skogberg@futurice.com>
2023-02-23 12:37:57 +0200

    Permission

--- cdk/src/main/scala/com/malliina/cdk/LambdaStack.scala
+++ cdk/src/main/scala/com/malliina/cdk/LambdaStack.scala
@@ -1,13 +1,14 @@
 package com.malliina.cdk
 
 import software.amazon.awscdk.{Duration, Stack}
-import software.amazon.awscdk.services.lambda.{CfnParametersCode, Code, FunctionAttributes, IFunction, Function as LambdaFunction, Runtime as LambdaRuntime}
+import software.amazon.awscdk.services.lambda.{CfnParametersCode, Code, FunctionAttributes, IFunction, Permission, Function as LambdaFunction, Runtime as LambdaRuntime}
 import software.amazon.awscdk.services.logs.{CfnSubscriptionFilter, FilterPattern, ILogGroup, LogGroup, RetentionDays, SubscriptionFilter, SubscriptionFilterOptions}
 import software.amazon.awscdk.services.logs.destinations.LambdaDestination
 import software.constructs.Construct
 
 class LambdaStack(scope: Construct, val constructId: String)
-  extends Stack(scope, constructId, CDK.stackProps):
+  extends Stack(scope, constructId, CDK.stackProps)
+  with CDKSyntax:
   val stack = this
   val code = CfnParametersCode.Builder.create().build()
   val assetCode = Code.fromAsset("jartarget")
@@ -21,12 +22,13 @@ class LambdaStack(scope: Construct, val constructId: String)
     .build()
   val streamLambda =
     LambdaFunction.fromFunctionName(stack, "StreamFuncNamed", "LogsToElasticsearch_search")
-//  val filter = SubscriptionFilter.Builder
-//    .create(stack, "Filter")
-//    .logGroup(function.getLogGroup)
-//    .destination(LambdaDestination.Builder.create(streamLambda).build())
-//    .filterPattern(FilterPattern.spaceDelimited("timestamp", "level", "logger", "message"))
-//    .build()
+  val streamPermission = Permission
+    .builder()
+    .principal(principal("logs.amazonaws.com"))
+    .action("lambda:InvokeFunction")
+    .sourceArn(function.getLogGroup.getLogGroupArn)
+    .build()
+  streamLambda.addPermission("StreamPermission", streamPermission)
   function.getLogGroup.addSubscriptionFilter(
     "SubscriptionFilter",
     SubscriptionFilterOptions
